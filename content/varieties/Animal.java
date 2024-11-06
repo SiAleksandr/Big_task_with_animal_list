9f3459c1c89d942de00a895f422757113fd24059
Sikalov_Aleksandr <sikalov-aleksandr@mail.ru>
2024-11-06 22:40:52 +0300

    Работа началась, создан абстрактный класс

---
+++ content/varieties/Animal.java
@@ -0,0 +1,9 @@
+package content.varieties;
+
+public abstract class Animal {
+    Integer groupId;
+    public Animal() {};
+    public void setId(Integer groupNumber) {
+        groupId = groupNumber;
+    }
+}
